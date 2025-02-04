5b62798ed0cabe43f8638115ba9e3dd89a3baefd
Burhan Dündar <burhan.dundar@ykteknoloji.com.tr>
2025-02-04 21:25:13 +0300

    Initial Commit

---
+++ MVVMArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMArchitecture
+//
+//  Created by Yapı Kredi Teknoloji A.Ş. on 4.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
